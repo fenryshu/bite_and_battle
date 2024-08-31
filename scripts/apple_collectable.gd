41c66c203d3a5b65c114e072c4db231912595e6a
fenryshu <dongoctram1404@gmail.com>
2024-08-31 17:18:43 +0200

    done_collecting_apples

---
+++ scripts/apple_collectable.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+
+func _ready() -> void:
+	fallfromtree()
+	
+func fallfromtree():
+	$AnimationPlayer.play("fallingfromtree")
+	await get_tree().create_timer(1.5).timeout
+	$AnimationPlayer.play("fade")
+	print("+1 apples")
+	await get_tree().create_timer(0.3).timeout
+	queue_free()
